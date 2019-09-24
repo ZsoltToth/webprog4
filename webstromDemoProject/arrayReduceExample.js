b9dd7cd37c2ba68ecb3aaed0286dc047204f3908
Zsolt Toth <tothzs87@gmail.com>
2019-09-24 09:06:33 +0200

    arrayReduceExample.js array was reduced with sum
    reduce function was demonstrated.

--- webstromDemoProject/arrayReduceExample.js
+++ webstromDemoProject/arrayReduceExample.js
@@ -3,4 +3,10 @@ var students = [
     {name : 'Bob', score : 57},
     {name : 'Charlie', score : 82}
 
-]
+];
+
+console.log(
+    students.reduce((total,current)=>{
+        return total + current.score
+    },0)
+);
