0a0a12fa4b86739fae9110b12c80ce0810ba78f9
Zsolt Toth <tothzs87@gmail.com>
2019-11-19 09:14:08 +0100

    react_example
    ShoppingCart - greetings work conditionally

--- react_example/src/App.js
+++ react_example/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <ShoppingCart name="zsolt"/>
+          <ShoppingCart/>
         </p>
         <a
           className="App-link"
--- react_example/src/ShoppingCart.js
+++ react_example/src/ShoppingCart.js
@@ -8,7 +8,13 @@ class ShoppingCart extends React.Component{
 
     render(){
         return (
-            <p>Hello {this.props.name}</p>);
+            <p>Hello
+                {this.props.name !== undefined &&
+            this.props.name}
+            {this.props.name === undefined &&
+            "World"}
+                /*{this.props.name !== undefined ? this.props.name : "World"}*/
+            </p>);
     }
 }
 
